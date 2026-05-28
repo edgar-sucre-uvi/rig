f50d94970d342e9e6d92bdf10c6616b3f200bbcb
edgar-sucre-uvi <edgar.sucre@uniqueglobalinc.com>
2026-05-28 07:40:47 -0500

    update example

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,6 +2,10 @@ package domain
 
 import "context"
 
+/* ========================================================================== */
+/*                  Example interface, to test the generator                  */
+/* ========================================================================== */
+
 type User struct {
 	ID   int
 	Name string
@@ -9,6 +13,7 @@ type User struct {
 
 type UserRepository interface {
 	GetUser(ctx context.Context, id int) (*User, error)
+	CreateUSer(ctx context.Context, name string, age int) error
 }
 
-//go:generate rig-mock -type=UserRepository -outdir=../test/mocks -outfile=user_repository_mock.go
+//go:generate rig-mock -type=UserRepository -outdir=../mocks -outfile=user_repository_mock.go
